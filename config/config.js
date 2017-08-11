53ada8d74fa7f20d0a7f8d42da611c1f273ced4e
Don Knapp <don@dknapp.com>
2017-08-10 21:53:33 -0400

    Roughed in the database.

---
+++ config/config.js
@@ -0,0 +1,26 @@
+
+const config = {
+  "development": {
+    "username": "root",
+    "password": "root",
+    "database": "burger1",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "root",
+    "database": "burger1",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "root",
+    "database": "burger1",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 'use strict';
 
+const port = process.env.PORT || 3000;
+
 const express = require( "express" );
 const bodyparser = require( "body-parser" );
 const exphbs = require( "express-handlebars" );
 const methoverride = require( "method-override" );
+const expstatic = require( "express-static" );
 const mysql = require( "mysql" );
 
 const app = express();
 
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+// app.use(expstatic(__dirname + '/public'));
+
+app.set('view engine', 'handlebars');
+
+app.get( "/", function( req, res ) {
+    console.log( "GET:/" );
+    mydb.getAll( function( burgerList ) {
+        res.render( "burgers.handlebars", {
+            data: "somedata",
+            burgers: burgerList
+        });
+        res.redirect( "/" );
+    })
+    // res.json( { status: "OK" } );
+});
+
+app.post( "/add", function( req, res ) {
+    console.log( req.body );
+    res.json( { status: "OK" } );
+})
+
+app.delete( "/:id", function( req, res ) {
+    console.log( "delete:" + req.params.id );
+    mydb.deleteById( req.params.id );
+    res.redirect( "/" );
+})
+
+app.listen( port, function() {
+    console.log( "Listening on " + port );
+})
