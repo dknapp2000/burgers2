8b975d9823e994a765dcabf7d9a2cfb6bcc07d19
Don Knapp <don@dknapp.com>
2017-08-13 20:55:49 -0400

    First working copy.

--- s_server.js
+++ s_server.js
@@ -7,9 +7,9 @@ const bodyparser = require( "body-parser" );
 const exphbs = require( "express-handlebars" );
 const methoverride = require( "method-override" );
 const expstatic = require( "express-static" );
-// const mydb = require( "./db/mydb.js" );
+// const mydb = require( "./db/mydb.js" );  // Switiching to sequelize
 
-const db = require( "./models" );
+const sqdb = require( "./models/sqdb.js" );
 
 const app = express();
 
@@ -21,33 +21,28 @@ app.set('view engine', 'handlebars');
 
 app.get( "/", function( req, res ) {
     console.log( "GET:/" );
-    mydb.getAllBurgers( function( burgerList ) {
+    sqdb.getAllBurgers( function( burgerList ) {
         console.log( burgerList );
-        // res.json( burgerList );
         res.render( "burgers.handlebars", {
             data: "somedata",
             burgers: burgerList
         });
-    })
+    });
 });
 
 app.post( "/add", function( req, res ) {
-    console.log( req.body );
-    mydb.addOneBurger( req.body.burgername, function() {
+    console.log( "/ADD, body = ", req.body );
+    sqdb.addOneBurger( req.body.burgerName, function() {
+        console.log( res.redirect );
         res.redirect( "/" );
     })
 })
 
 app.post( "/devour/:id", function( req, res ) {
-    mydb.setDevoured( req.params.id, function() {
+    console.log( "/devour: " + req.params.id )
+    sqdb.devourOneBurger( req.params.id, function() {
         res.redirect("/")
     });
-})
-
-app.delete( "/:id", function( req, res ) {
-    console.log( "delete:" + req.params.id );
-    mydb.deleteById( req.params.id );
-    res.redirect( "/" );
 })
 
 app.listen( port, function() {
